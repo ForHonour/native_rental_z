a237a845b0f0ed83e119f1c47bbe5540ff0bb5be
khang-nghiem <31397614+khang-nghiem@users.noreply.github.com>
2021-11-20 17:08:15 +0700

    Native RentalZ done

--- app/src/main/java/com/example/nativerentalz/MainActivity.kt
+++ app/src/main/java/com/example/nativerentalz/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.nativerentalz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
